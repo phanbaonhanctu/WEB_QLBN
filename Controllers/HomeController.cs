e5cb2e07a14e07b9887b07e290cc7d28bdc109c4
B1805900 <nhanb1805900@student.ctu.edu.vn>
2024-01-25 18:14:57 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WEB_QLBN.Models;
+
+namespace WEB_QLBN.Controllers
+{
+    public class HomeController : Controller
+    {
+        string apiUrl = "http://api.ctu-it.com:5000/api/Benhnhan";
+        List<Benhnhan> benhnhans = new List<Benhnhan>();
+
+        async Task<List<Benhnhan>> Getdata()
+        {
+            List<Benhnhan> benhnhanList = new List<Benhnhan>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonString = await response.Content.ReadAsStringAsync();
+                    benhnhanList = JsonConvert.DeserializeObject<List<Benhnhan>>(jsonString);
+                }
+            }
+            
+            return benhnhanList;
+        }
+
+        // GET: Home
+        public async Task<ActionResult> Index()
+        {
+            benhnhans = await Getdata();
+            return View(benhnhans);
+        }
+    }
+}
---
+++ Models/Benhnhan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB_QLBN.Models
+{
+    public class Benhnhan
+    {
+        public Guid id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public string gender { get; set; }
+        public DateTime time { get; set; }
+        public string ppdt { get; set; }
+    }
+}
